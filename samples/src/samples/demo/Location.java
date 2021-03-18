5944dea6830e8d9b7706993f0b64ac94e195f5a5
Aliah Aniqah <aliahaniqah@gmail.com>
2021-03-18 20:36:41 +0800

    third commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,13 @@
+package samples.demo;
+
+public class Location {
+   private String name;
+   
+   void setName(String name) {
+	   this.name=name;
+   }
+   
+   String getName() {
+	   return name;
+   }
+}

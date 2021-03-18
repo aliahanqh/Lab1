817e1320e16b38aad8948753ed33fac3a6d9e0a6
Aliah Aniqah <aliahaniqah@gmail.com>
2021-03-19 00:03:05 +0800

    Final Commit

--- Lab1/src/Exercise7/Main.java
+++ Lab1/src/Exercise7/Main.java
@@ -4,7 +4,7 @@ import Exercise8.list;
 
 public class Main {
 	public static void main(String args[]) {
-		WeightOnMars wom = new WeightOnMars(110);
+		WeightOnMars wom = new WeightOnMars(100);
 		wom.calculateWeight();
 		list list = new list();
 	 }
--- Lab1/src/Exercise7/WeightOnMars.java
+++ Lab1/src/Exercise7/WeightOnMars.java
@@ -16,8 +16,7 @@ public class WeightOnMars {
 		double earthWeight = mass * 9.81;
 		double marsWeight = mass * 3.71;
 
-		System.out.println("Mass: "+ mass +"kg\nWeight on Earth: " + earthWeight +"N\nWeight on Mars: "+ marsWeight +"N");
-		System.out.println("\nMass: "+ mass +"kg\nWeight on Earth: " + earthWeight +"N\nWeight on Mars: "+ marsWeight +"N\n");
+		System.out.println("Mass: "+ mass +"kg\nWeight on Earth: " + earthWeight +"N\nWeight on Mars: "+ marsWeight +" N\n");
 	}
 	
 }
